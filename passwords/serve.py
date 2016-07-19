dd88fbcf3f5c3433a22fa462ed98b5a203b8d619
James McKay <code@jamesmckay.net>
2016-07-19 13:46:40 +0100

    The basic API, with an implement of the "store password" method

--- passwords/serve.py
+++ passwords/serve.py
@@ -4,7 +4,9 @@ import os
 import bcrypt
 import hashlib
 
-from flask import Flask
+from datetime import datetime
+from flask import Flask, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
@@ -15,15 +17,87 @@ def hello():
 GLOBAL_SALT = os.environ['GLOBAL_SALT']
 WORK_FACTOR = int(os.environ['WORK_FACTOR'])
 
+# ====== Password hashing ====== #
+
+'''
+Passwords are hashed twice, using two separate algorithms.
+
+The first algorithm is a SHA-512 hash of the password, combined with the global
+salt. This serves two purposes. First, it gets round bcrypt's upper length limit
+of 72 characters; second, it adds a "secret" independent of the database to
+prevent brute forcing by anyone who does not already know the secret.
+
+The second algorithm is bcrypt, which does the heavy lifting, while also providing
+a password-specific salt.
+'''
+
 def __prehash(pwd):
-    return hashlib.sha512(GLOBAL_SALT + pwd).digest()
+    return hashlib.sha512((pwd + GLOBAL_SALT).encode()).digest()
 
 def __hash_password(pwd):
     salt = bcrypt.gensalt(WORK_FACTOR)
     return bcrypt.hashpw(__prehash(pwd), salt)
 
 def __test_password(pwd, stored):
-    return bcrypt.hashpw(__prehash(pwd), stored) == stored
+    encoded_stored = stored.encode()
+    return bcrypt.hashpw(__prehash(pwd), encoded_stored) == encoded_stored
+
+
+# ====== CRUD operations ====== #
+
+def __get_collection():
+    client = MongoClient('passwords-db', 27017)
+    db = client.passwords
+    return db.passwords
+
+def __get_record(id):
+    collection = __get_collection()
+    return collection.find_one({'_id': id})
+
+def __add_record(record):
+    collection = __get_collection()
+    return collection.insert_one(record).inserted_id
+
+def __delete_record(id):
+    collection = __get_collection()
+    collection.delete_one({'_id': id})
+
+
+# ====== Stores a password ====== #
+
+
+'''
+Stores a password in the database, and returns its ID.
+'''
+@app.route('/password', methods=['POST'])
+def set_password():
+    pwd = request.form['password']
+    hashed = __hash_password(pwd)
+    model = {
+        'hash': hashed,
+        'date': datetime.utcnow()
+    }
+    return str(__add_record(model))
+
+
+# ====== Deletes a password from the database ====== #
+
+'''
+Deletes the password with specified ID from the database.
+'''
+@app.route('/password/<id>', methods=['DELETE'])
+def delete_password(id):
+    pass
+
+
+# ====== Tests a password ====== #
+
+'''
+Tests the password with given ID. Returns true if the password is correct, otherwise false.
+'''
+@app.route('/password/test/<id>', methods=['POST'])
+def test_password(id):
+    pwd = request.form['password']
 
 
 if __name__ == '__main__':
